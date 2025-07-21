3cf5bd564e02f2e9c23b015ede938c1a2643728b
I_Bobreshova <sotapet0215@gmail.com>
2025-07-21 14:25:26 +0300

    final update :0 tasks-02.js

--- js-tasks/tasks-02.js
+++ js-tasks/tasks-02.js
@@ -1,26 +1,26 @@
 /*
 _task.1:
-function goals (laLigaGoals, copaDelReyGoals, championsLeagueGoals) {
-  return laLigaGoals+copaDelReyGoals+championsLeagueGoals;
+function goals(laLigaGoals, copaDelReyGoals, championsLeagueGoals) {
+  return laLigaGoals + copaDelReyGoals + championsLeagueGoals;
 }
 _task.2:
 function makeNegative(num) {
-  return (num > 0) ? -num : num;
+  return num > 0 ? -num : num;
 }
 _task.3:
-function move (position, roll) {
+function move(position, roll) {
   return position + roll * 2;
 }
 _task.4:
 function greet(name, owner) {
-  return (name === owner) ? 'Hello boss' : 'Hello guest';
+  return name === owner ? "Hello boss" : "Hello guest";
 }
 _task.5:
 function litres(time) {
   return Math.floor(time * 0.5);
 }
 _task.6:
-function lovefunc(flower1, flower2){
-  return  (flower1%2 !== flower2%2);
+function lovefunc(flower1, flower2) {
+  return flower1 % 2 !== flower2 % 2;
 }
 */
