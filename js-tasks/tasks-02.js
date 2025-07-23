5e32d2014330f65a0162abba732916466f6e94ec
Iryna-Mazurak <45284879+Iryna-Mazurak@users.noreply.github.com>
2025-07-23 12:04:30 +0000

    Add commented links

---
+++ js-tasks/tasks-02.js
@@ -0,0 +1,19 @@
+// https://www.codewars.com/kata/convert-a-string-to-an-array/train/javascript
+
+// https://www.codewars.com/kata/dna-to-rna-conversion/train/javascript
+
+// https://www.codewars.com/kata/577a98a6ae28071780000989/train/javascript
+
+// https://www.codewars.com/kata/544a54fd18b8e06d240005c0/train/javascript
+
+// https://www.codewars.com/kata/53ee5429ba190077850011d4/train/javascript
+
+// https://www.codewars.com/kata/5b853229cfde412a470000d0/train/javascript
+
+// https://www.codewars.com/kata/5933a1f8552bc2750a0000ed/train/javascript
+
+// https://www.codewars.com/kata/574b3b1599d8f897470018f6/train/javascript
+
+// https://www.codewars.com/kata/55f9bca8ecaa9eac7100004a/train/javascript
+
+// https://www.codewars.com/kata/5545f109004975ea66000086/train/javascript
