f0f2d0280e14d32f95b571dc9ec0b2e34a5f2398
MaksymSavchuk780 <eldenking2708@gmail.com>
2025-07-25 13:43:03 +0000

    add js tasks

--- js-tasks/tasks-03.js
+++ js-tasks/tasks-03.js
@@ -1,8 +1,19 @@
 // https://www.codewars.com/kata/geometry-basics-circle-circumference-in-2d/train/javascript
-
+const circleCircumference = (circle) => 2 * Math.PI * circle.radius;
 
 // https://www.codewars.com/kata/training-js-number-12-loop-statement-for-dot-in-and-for-dot-of/train/javascript
-
+function giveMeFive(obj) {
+  const result = [];
+  for (const key in obj) {
+    if (key.length === 5) {
+      result.push(key);
+    }    
+    if (obj[key].length === 5) {
+      result.push(obj[key]);
+    }
+  }
+  return result;
+}
 
 // https://www.codewars.com/kata/understanding-closures-the-basics/train/javascript
 
