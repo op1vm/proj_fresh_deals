77586e2573771d6e40aef6546744aced9ea7a922
MaksymSavchuk780 <eldenking2708@gmail.com>
2025-07-25 13:50:22 +0000

    add third js task

--- js-tasks/tasks-03.js
+++ js-tasks/tasks-03.js
@@ -16,7 +16,13 @@ function giveMeFive(obj) {
 }
 
 // https://www.codewars.com/kata/understanding-closures-the-basics/train/javascript
-
+function buildFun(n) {
+  const funcs = [];
+  for (let i = 0; i < n; i++) {
+    funcs.push(() => i);
+  }
+  return funcs;
+}
 
 // https://www.codewars.com/kata/fun-with-es6-classes-number-2-animals-and-inheritance/train/javascript
 
