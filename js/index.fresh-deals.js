eb7fbbae403031014ceadca44b40fcf6c43d37ec
DianaPatynok <patynok.d@gmail.com>
2025-08-06 18:23:24 +0300

    Edit fresh-deals

--- js/index.fresh-deals.js
+++ js/index.fresh-deals.js
@@ -1,5 +1,3 @@
-//import { CartModal } from "./cart-modal.js";
-//import { showAlert } from "./alert.js";
 const tabs = document.querySelectorAll(".fresh-deals__tab");
 const productGroups = document.querySelectorAll(".fresh-deals__products");
 
@@ -28,34 +26,31 @@ const favorites = JSON.parse(localStorage.getItem("favorites")) || [];
 
 document.querySelectorAll(".favorite-btn").forEach((btn) => {
   const productName = btn.dataset.name;
+  const product = btn.closest(".fresh-deals__product");
+  const notification = product.querySelector(".notification");
+
   if (favorites.includes(productName)) {
     btn.classList.add("favorited");
   }
   btn.addEventListener("click", () => {
     const index = favorites.indexOf(productName);
+
     if (index !== -1) {
       favorites.splice(index, 1);
       btn.classList.remove("favorited");
     } else {
       favorites.push(productName);
       btn.classList.add("favorited");
-    }
 
+      if (notification) {
+      notification.classList.add("notification--visible");
+      setTimeout(() => {
+      notification.classList.remove("notification--visible");
+      }, 1800);
+    }}
     localStorage.setItem("favorites", JSON.stringify(favorites));
-  });
-});
-
-const favoriteBtn = document.querySelector(".favorite-btn");
-const notification = document.getElementById("favorite-notification");
-
-favoriteBtn.addEventListener("click", () => {
-  notification.classList.add("notification--visible");
-
-  setTimeout(() => {
-    notification.classList.remove("notification--visible");
-  }, 3000);
+  })
 })
-
 
   
 
