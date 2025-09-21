3a2207f116b1f357b17bf16a7cceac50e636506d
I_Bobreshova <sotapet0215@gmail.com>
2025-09-21 11:28:34 +0000

    damn

--- js/products.js
+++ js/products.js
@@ -1,14 +1,17 @@
+
 let productsData = {};
+
 
 export async function loadProducts() {
   const response = await fetch('/api/products.json');
   const productsArray = await response.json();
   productsData = {};
   productsArray.forEach(product => {
-    productsData[product.id] = product;
+    productsData[String(product.id)] = product;
   });
 }
 
+
 export function getProductById(id) {
-  return productsData[id] || null;
+  return productsData[String(id)] || null;
 }
